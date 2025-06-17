08e99342dec785736abec58ff2af1c46073297b3
Yokub <yoqub.madixanov@outlook.com>
2025-06-17 17:24:45 +0500

    Adding toLocaleString to balance and exchanges

--- src/layouts/stats-page-layout/UI/DashBoard.jsx
+++ src/layouts/stats-page-layout/UI/DashBoard.jsx
@@ -23,8 +23,8 @@ const DashBoard = () => {
                             <img src={player.img} alt={player.name} />
                         </div>
                         <h2>{player.name}</h2>
-                        <p>BALANCE: <span>{player.balance}</span></p>
-                        <p>EXCHANGES: <span>{player.exchanges}</span></p>
+                        <p>BALANCE: <span>{player.balance.toLocaleString('ru-RU')}</span></p>
+                        <p>EXCHANGES: <span>{player.exchanges.toLocaleString('ru-RU')}</span></p>
                     </div>
                 );
             })}
