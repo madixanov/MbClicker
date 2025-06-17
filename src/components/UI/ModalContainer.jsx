121298efe60b4a652ebefb9f190c26e4bc4fc2b9
Yokub <yoqub.madixanov@outlook.com>
2025-06-17 16:23:16 +0500

    Replcaing Close Icon to Png

--- src/components/UI/ModalContainer.jsx
+++ src/components/UI/ModalContainer.jsx
@@ -1,13 +1,14 @@
-import { IoCloseOutline } from "react-icons/io5";
 import './modal-container.css'
-
+import close from '../../assets/icons/delete-sign.png'
 
 const ModalContainer = ({ title, content, closeModal }) => {
     return (
         <div className="modal-container">
             <div className="modal-title">
                 <h1 className="modal-title-h1">{title}</h1>
-                <button onClick={closeModal}className="modal-title-button"><IoCloseOutline /></button>
+                <button onClick={closeModal}className="modal-title-button">
+                    <img src={close} alt="" />
+                </button>
             </div>
             {content}
         </div>
