bdb6253988e780a6614ba38f647d8a812ade7be2
Yokub <yoqub.madixanov@outlook.com>
2025-06-19 15:17:19 +0500

    Adding clicks and levels to Strapi

--- src/layouts/home-page-layout/UI/MbCounter.jsx
+++ src/layouts/home-page-layout/UI/MbCounter.jsx
@@ -3,19 +3,20 @@ import exchange from "../../../assets/icons/exchange.svg";
 import exchange1 from "../../../assets/icons/exchange1.svg";
 
 import useMbStore from "../../../store/mb-store.js";
+import usePlayerData from '../../../hooks/usePlayerData.js'
 import { useNavigate } from "react-router";
 import { useEffect, useState, useRef } from "react";
 import { animate } from "framer-motion";
 
 const MbCounter = () => {
+  const { player } = usePlayerData();
   const mbCountAll = useMbStore((state) => state.mbCountAll);
-  const [animatedCount, setAnimatedCount] = useState(mbCountAll);
+  const [animatedCount, setAnimatedCount] = useState(player.clicks);
 
   const navigate = useNavigate();
-  const prevCountRef = useRef(mbCountAll); // запоминаем предыдущее значение
+  const prevCountRef = useRef(mbCountAll); 
 
   useEffect(() => {
-    // избегаем повторной анимации, если значение не изменилось
     if (prevCountRef.current === mbCountAll) return;
 
     const controls = animate(prevCountRef.current, mbCountAll, {
