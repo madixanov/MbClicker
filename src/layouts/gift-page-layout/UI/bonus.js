acc3d4622c78aabdbeb3e4675745d234e1ffac53
Yokub <yoqub.madixanov@outlook.com>
2025-06-24 17:20:16 +0500

    Debugging strapi

---
+++ src/layouts/gift-page-layout/UI/bonus.js
@@ -0,0 +1,9 @@
+const BONUS_LINKS = {
+  youtube: "https://youtube.com/@yourchannel",
+  telegram: "https://t.me/yourchannel",
+  instagram: "https://instagram.com/yourprofile",
+  vk: "https://vk.com/yourgroup",
+  tiktok: "https://tiktok.com/@yourprofile",
+};
+
+export default BONUS_LINKS;
