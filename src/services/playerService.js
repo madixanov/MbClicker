41686bdaded21e17243172fa1689bbeeac2aa21c
Yokub <yoqub.madixanov@outlook.com>
2025-06-24 16:38:54 +0500

    Replacing fetch to axios

--- src/services/playerService.js
+++ src/services/playerService.js
@@ -1,44 +1,31 @@
+import axios from "axios";
 import { API_BASE_URL } from "../config/api";
 
-// ✅ Получить игрока по telegram_id
 export const fetchPlayerByTelegramId = async (telegram_id) => {
-  const url = new URL(`${API_BASE_URL}/players`);
-  url.searchParams.append("filters[telegram_id][$eq]", telegram_id);
-  url.searchParams.append("publicationState", "preview");
+  const res = await axios.get(`${API_BASE_URL}/players`, {
+    params: {
+      filters: {
+        telegram_id: {
+          $eq: telegram_id,
+        },
+      },
+      publicationState: "preview",
+    },
+  });
 
-  const res = await fetch(url.toString(), { method: "GET" });
-  if (!res.ok) throw new Error("Ошибка при получении игрока");
-
-  const data = await res.json();
-  return data?.data?.[0] || null;
+  return res.data?.data?.[0] || null;
 };
 
-// ✅ Обновить игрока по documentId
 export const updatePlayer = async (documentId, fields = {}) => {
-  const res = await fetch(`${API_BASE_URL}/players/${documentId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data: fields }),
+  return axios.put(`${API_BASE_URL}/players/${documentId}`, {
+    data: fields,
   });
-
-  if (!res.ok) throw new Error("Ошибка при обновлении игрока");
-  return res.json();
 };
 
-// ✅ Создать нового игрока
 export const createPlayer = async (playerData) => {
-  const res = await fetch(`${API_BASE_URL}/players`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ data: playerData }),
+  return axios.post(`${API_BASE_URL}/players`, {
+    data: playerData,
   });
-
-  if (!res.ok) throw new Error("Ошибка при создании игрока");
-  return res.json();
 };
 
 // ✅ Отложенное обновление при offline/ошибке
