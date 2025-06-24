77523c813b30aecbacc4d856b2ebc25d0aedd7c1
Yokub <yoqub.madixanov@outlook.com>
2025-06-24 12:37:47 +0500

    Optimizing projects, increasing performance

--- src/layouts/home-page-layout/UI/Avatar.jsx
+++ src/layouts/home-page-layout/UI/Avatar.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import avatar from "../../../assets/images/avatar.png";
+import avatar from "../../../assets/images/avatar.webp";
 import useMbStore from "../../../store/mb-store";
 
 const Avatar = () => {
