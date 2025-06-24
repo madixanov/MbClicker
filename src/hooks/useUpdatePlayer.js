1caa957fc7f1130edcccff2c694e2b3671ad4a9e
Yokub <yoqub.madixanov@outlook.com>
2025-06-24 12:30:30 +0500

    Optimizing projects, increasing performance

--- src/hooks/useUpdatePlayer.js
+++ src/hooks/useUpdatePlayer.js
@@ -4,23 +4,13 @@ import { fetchPlayerByTelegramId, updatePlayer } from "../services/playerService
 const useUpdatePlayer = () => {
   return async (fields) => {
     const user = getTelegramUser();
-    if (!user) {
-      console.warn("❌ Пользователь Telegram не найден");
-      return;
-    }
 
     const player = await fetchPlayerByTelegramId(user.id);
-
-    if (!player || !player.documentId) {
-      console.warn("⚠️ Игрок не найден или нет documentId");
-      return;
-    }
 
     try {
       await updatePlayer(player.documentId, {
         data: fields
       });
-      console.log("✅ Игрок обновлён:", fields);
     } catch (err) {
       console.error("❌ Ошибка при обновлении игрока:", err.response?.data || err);
     }
