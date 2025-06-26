e6097b28323a1262087c28eb157908887dc7f575
Yokub <yoqub.madixanov@outlook.com>
2025-06-26 12:19:23 +0500

    Making functioning LeadersBoard

--- src/layouts/stats-page-layout/UI/LeadersList.jsx
+++ src/layouts/stats-page-layout/UI/LeadersList.jsx
@@ -1,8 +1,29 @@
-import { useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import players from "../leaders-list";
+import { fetchLeaderboardPlayers } from "../../../services/playerService";
 
 const PlayerCard = ({ player, index }) => {
     const placement = index + 1;
+    const [ players, setPlayers ] = useState([]);
+    const defaultAvatar = `data:image/svg+xml;utf8,
+        <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+            <rect fill="white" width="100" height="100"/>
+            <circle cx="50" cy="35" r="20" fill="%23ccc"/>
+            <rect x="25" y="60" width="50" height="25" rx="10" fill="%23ccc"/>
+        </svg>`;
+
+    useEffect(() => {
+        const loadPlayers = async () => {
+            try {
+                const data = await fetchLeaderboardPlayers();
+                setPlayers(data);
+            } catch (error) {
+                console.error("Ошибка при загрузке игроков:", error);
+            }
+        }
+
+        loadPlayers();
+    }, []);
 
     return (
         <div className="player-container" key={player.name}>
@@ -10,26 +31,29 @@ const PlayerCard = ({ player, index }) => {
                 <p className="placement">{placement}</p>
                 <div className="player-info">
                     <div className="leaders-photo-container">
-                        <img src={player.img} alt={player.name} />
+                        <img
+                                src={player.photo_url || defaultAvatar}
+                                alt={player.name}
+                                onError={(e) => {
+                                    e.target.onerror = null;
+                                    e.target.src = defaultAvatar;
+                                }}
+                                style={{ backgroundColor: "white" }}
+                            />
                     </div>
                     <p>{player.name}</p>
                 </div>
             </div>
-            <p className="balance">{player.balance.toLocaleString('ru-RU')}</p>
-            <p className="exchanges">{player.exchanges.toLocaleString('ru-RU')}</p>
+            <p className="balance">{player.clicks.toLocaleString('ru-RU')}</p>
+            <p className="exchanges">0</p>
         </div>
     );
 };
 
 const LeadersList = () => {
-    const sortedByBalance = useMemo(
-        () => [...players].sort((a, b) => b.balance - a.balance),
-        []
-    );
-
     return (
         <div className="leaders-list">
-            {sortedByBalance.map((player, index) => (
+            {players.map((player, index) => (
                 <PlayerCard key={player.name} player={player} index={index} />
             ))}
         </div>
