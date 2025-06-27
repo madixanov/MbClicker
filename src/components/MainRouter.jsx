36ed7329c99c71167ceaf94af1cd7091c812bc01
Нщлги <yoqub.madixanov@outlook.com>
2025-06-27 13:48:58 +0500

    Dubugging bonus, now bonus is given only one time

--- src/components/MainRouter.jsx
+++ src/components/MainRouter.jsx
@@ -3,10 +3,9 @@ import { lazy, Suspense, useEffect } from "react";
 import LoadingPage from "../pages/LoadingPage";
 import AutoSaveClicks from "./AutoSaveClisk";
 import useTelegramAuth from "../hooks/useTelegramAuth";
-import { retryPendingUpdate } from "../services/playerService";
+import { retryPendingUpdate, updateReferralBonus } from "../services/playerService";
 import useSyncOnUnload from "../hooks/useSyncOnUnload";
 import useAppReady from "../hooks/useAppReady";
-import useReferralBonus from "../hooks/useReferralBonus";
 
 // Lazy загрузка страниц
 const HomePage = lazy(() => import("../pages/HomePage"));
@@ -19,7 +18,7 @@ const FriendsPage = lazy(() => import("../pages/FriendsPage"));
 const MainRouter = () => {
   useTelegramAuth();
   useSyncOnUnload();
-  useReferralBonus();
+  updateReferralBonus();
   const appReady = useAppReady();
 
   useEffect(() => {
