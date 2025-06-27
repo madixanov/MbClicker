1d65238490d206f502149bb231766920aaa6faa6
Нщлги <yoqub.madixanov@outlook.com>
2025-06-27 13:46:49 +0500

    Dubugging bonus, now bonus is given only one time

--- src/layouts/friends-page-layout/UI/FriendsList.jsx
+++ src/layouts/friends-page-layout/UI/FriendsList.jsx
@@ -1,7 +1,25 @@
 import tg from '../../../assets/icons/tg.png'
+import { useState, useEffect } from "react";
+import { fetchPlayerWithFriends } from "../../../services/playerService";
+import usePlayerStore from "../../../store/player-store";
+import useMbStore from "../../../store/mb-store";
 
 const FriendsList = () => {
     const bgColors = ['#D9FF00', '#FFC839', '#FF0000' ]
+    const [ friends, setFriends ] = useState([]);
+    const { player } = usePlayerStore();
+
+    useEffect(() => {
+        const loadFriends = async () => {
+            if (!player) return;
+
+            const updatePlayer = await fetchPlayerWithFriends(player.telegram_id);
+            const friendsList = updatePlayer?.invited_friends || [];
+            setFriends(friendsList)
+        }
+
+        loadFriends();
+    }, [player])
 
     return (
         <div className="friends-list">
