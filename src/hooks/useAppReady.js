98a35445fec7ffc9439975e9a558be70a02b8719
Yokub <yoqub.madixanov@outlook.com>
2025-07-03 16:33:52 +0500

    Deleting unused loadplayer

--- src/hooks/useAppReady.js
+++ src/hooks/useAppReady.js
@@ -1,16 +1,13 @@
 import { useState, useEffect } from "react";
 import useLvlStore from "../store/lvl-store";
 import useMbStore from "../store/mb-store";
-import usePlayerData from "./usePlayerData";
 
 const useAppReady = () => {
   const [ready, setReady] = useState(false);
-  const { loadPlayer } = usePlayerData(); // ⬅️ добавлено
 
   useEffect(() => {
     const load = async () => {
       try {
-        await loadPlayer(); // ⬅️ загружаем игрока
         await useLvlStore.getState().loadLevelFromStrapi();
         await useMbStore.getState().loadMbFromPlayer();
       } catch (e) {
--- src/pages/StatsPage.jsx
+++ src/pages/StatsPage.jsx
@@ -1,21 +1,18 @@
 import { lazy, useEffect } from "react";
 import '../layouts/stats-page-layout/stats-page.css'
-import usePlayerData from "../hooks/usePlayerData";
 
 const Header = lazy(() => import('../components/Header'))
 const Footer = lazy(() => import('../components/Footer'))
 const Main = lazy(() => import ('../layouts/stats-page-layout/Main'))
 
 const StatsPage = () => {
-    const { loadPlayer} = usePlayerData();
 
     useEffect(() => {
-            loadPlayer();
             document.body.classList.add("other-page")
             return () => {
                 document.body.classList.remove('other-page')
             }
-        }, [loadPlayer])
+        }, [])
 
     return (
         <div className="stats-page">
