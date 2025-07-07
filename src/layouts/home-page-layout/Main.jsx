56447b79fac442adf0cbee4a461b47157163ede9
Yokub <yoqub.madixanov@outlook.com>
2025-07-07 13:01:47 +0500

    Changing Loading Page for tasks, gifts pages

--- src/layouts/home-page-layout/Main.jsx
+++ src/layouts/home-page-layout/Main.jsx
@@ -1,10 +1,7 @@
 import { lazy, Suspense } from "react";
 import './home-page.css';
-import LoadingPage from '../../pages/LoadingPage';
 import { Helmet } from "react-helmet";
 import avatar from '../../assets/images/avatar.webp';
-import Avatar from "./UI/Avatar";
-import PageLoading from "../../pages/PageLoading";
 
 const ProgressBar = lazy(() => import("./UI/ProgressBar"));
 const MbCounter = lazy(() => import("./UI/MbCounter"));
